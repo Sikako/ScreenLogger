a206748c5ad17ba4da73607b26c4b6d48d932ab7
Sikako <sikako.setp08@nctu.edu.tw>
2022-12-22 15:38:41 +0800

    更改ApiServer release 監聽Port 執行檔正常

--- PrtScApi/Program.cs
+++ PrtScApi/Program.cs
@@ -12,6 +12,7 @@ namespace PrtScApi
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+            builder.WebHost.UseUrls("http://localhost:5084", "https://localhost:7084");
 
             var app = builder.Build();
 
