fc36a5cf52b3d02b4534ecfffc1b12a6ae802972
Sikako <sikako.setp08@nctu.edu.tw>
2022-12-29 17:40:04 +0800

    優化程式 當Request不到API不會Error

--- source/screenshot.py
+++ source/screenshot.py
@@ -26,8 +26,10 @@ while True:
         
         files = {'imageFile': open(r'screenshot-'+ str(i) +'.png','rb')} # 傳圖片檔案
 
-        r = requests.post("https://notify-api.line.me/api/notify",
-                        headers=headers, params=params, files = files)
+        try:
+            r = requests.post("https://notify-api.line.me/api/notify", headers=headers, params=params, files = files)
+        except:
+            pass
 
         fileTest = "screenshot-"+ str(i-1) +".png" # 因為照片在傳出去的時候會鎖死刪不掉，所以要刪前一個檔案
         try: # 防止找不到檔案導致程式停止
